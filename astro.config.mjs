cdf489ac9e22ae960628c3ea45af9af8514bb69f
cferreras <cferreras@users.noreply.github.com>
2026-07-11 00:18:08 +0200

    Añade el paquete técnico SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
 
 const isNodeAdapter = process.env.ASTRO_ADAPTER === "node" || process.env.npm_lifecycle_event === "build:dokploy";
@@ -8,4 +9,13 @@ export default defineConfig({
   site: "https://mc.ferreras.dev",
   output: "server",
   adapter: isNodeAdapter ? node({ mode: "standalone" }) : vercel(),
+  integrations: [
+    sitemap({
+      namespaces: {
+        news: false,
+        video: false,
+        xhtml: false,
+      },
+    }),
+  ],
 });
